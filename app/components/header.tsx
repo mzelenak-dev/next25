e747c2eb1582a4f28e28174724a7ced1e4c22d0a
zedaenious <matt@zelenak.me>
2025-03-05 13:15:11 -0500

    adding core components to default layout

---
+++ app/components/header.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <Link href="./">Go back home</Link>
+    </>
+  )
+}
+
+export default Header;
